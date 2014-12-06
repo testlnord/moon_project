c4e3f93a8fb06240594b85656e878b012d535cfa
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-12-06 16:54:45 +0300

    solar

--- moon_scripts/solar_data.py
+++ moon_scripts/solar_data.py
@@ -1,9 +1,35 @@
 import csv
 from os import listdir
 from os.path import isfile, join, exists
+import re
 from subprocess import call
 
 LINK = 'http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2010/targzs/744860.tar.gz'
+
+def parse_dst(result):
+    path_to_files = "../data/dst"
+    if not exists(path_to_files):
+        call(["mkdir", path_to_files])
+        for y in range(2011, 2015):
+            for m in range(1, 13):
+                call([
+                    "wget",
+                    "http://www.aer.com/sites/default/files/Dst_%d_%02d.txt" % (y, m),
+                    "-O", path_to_files + "/" + "%d_%02d.txt" % (y,m)
+                ])
+
+    files = sum([list(open(path_to_files + "/" + f).readlines())[2:] for f in listdir(path_to_files) if isfile(join(path_to_files,f)) ], [])
+    files = filter(lambda x: re.match("\s+\d+-\d+-\d+", x), files)
+
+    grouped = dict()
+
+    for l in files:
+        m = re.match("\s+(\d+-\d+-\d+).*(-?\d+\.\d+)", l)
+        grouped.setdefault(m.group(1), []).append(float(m.group(2)))
+
+    for k,v in grouped.items():
+        result.setdefault(k, {})["dst"] = sum(v)/len(v)
+
 
 def load_solar_data():
     path_to_files = "../data/solar"
@@ -11,7 +37,7 @@ def load_solar_data():
     if not exists(path_to_files):
         call(["wget", LINK, '-O', 'solar.tar.gz'])
         call(["tar", "zxvf", 'solar.tar.gz'])
-        call(["mv", "744860", "../data/solar"])
+        call(["mv", "744860", path_to_files])
         call(["rm", "solar.tar.gz"])
 
     files = sum([list(open(path_to_files + "/" + f).readlines())[2:] for f in listdir(path_to_files) if isfile(join(path_to_files,f)) ], [])
@@ -27,8 +53,9 @@ def load_solar_data():
             grouped_by[row[0]] = []
         grouped_by[row[0]].append(dict(map(lambda x: (x, row[chosen_columns[x]]), chosen_columns.keys())))
 
-
     result = dict()
+
+    parse_dst(result)
 
     for k, v in grouped_by.items():
         result[k] = dict()
@@ -40,6 +67,4 @@ def load_solar_data():
 
 if __name__ == '__main__':
     qq = load_solar_data()
-    for l in qq.items():
-        print(l)
     print(len(qq))
