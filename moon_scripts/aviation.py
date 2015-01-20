b9dcba38e5f29903a1c1095c108e09fa2e74982b
Andronik Ordian <andronik.ordian@gmail.com>
2015-01-20 22:42:28 +0300

    add 0 accidents days

--- moon_scripts/aviation.py
+++ moon_scripts/aviation.py
@@ -1,5 +1,12 @@
 import csv
 from subprocess import call
+import datetime
+
+DATE_FORMAT = "%Y-%m-%d"
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
 
 def load_aviation_as_dict():
     call(['Rscript', 'aviation.R'])
@@ -8,5 +15,14 @@ def load_aviation_as_dict():
     result = {}
     for d, c in data:
         result[d] = c
+
+    # 0 accidents
+    start_date = datetime.datetime.strptime(min(result.keys()), DATE_FORMAT)
+    end_date   = datetime.datetime.strptime(max(result.keys()), DATE_FORMAT)
+    for n in daterange(start_date, end_date):
+        s = n.strftime(DATE_FORMAT)
+        if s not in result:
+            result[s] = 0
+
     call(['rm', output])
     return result
